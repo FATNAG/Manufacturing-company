7c96d045e2071a5112f4635a69ea28f47a9dbb76
odavema77 <odavema@gmail.com>
2021-10-02 21:45:43 -0500

    corrigiendo estilos

--- reok/src/pages/Sales.jsx
+++ reok/src/pages/Sales.jsx
@@ -7,37 +7,65 @@ function Sales() {
         <div className="Sales">
             <Navbar title = {"Informacion de Ventas"}/>
             <section id="section" class="intro-section">
+                <h3>Formulario de Pedido</h3>
+                <form action="#" method = "post" name = "gestion_ventas"className = "form-ventas">
+                    <main class= "contenedor-imput">
+                        <div class = "inputs">
+
                 <label for="numero de pedido">Numero de Pedido :</label>
                 <br />
                 <input type="number" name="numero de pedido" required="true" placeholder="numero de pedido" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="fecha de pedido">Fecha de Pedido</label>
                 <br />
                 <input type="date" name="fecha de pedido" required="true" placeholder="fecha de pedido" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="nombre cliente">Nombre Cliente </label>
                 <br />
                 <input type="text" name="nombre cliente" required="true" placeholder="Nombre Cliente" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="Id cliente">Identificacion Cliente</label>
                 <br />
                 <input type="number" name="Id cliente" required="true" placeholder="Identificacion Cliente" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="vendedor">Identificacion Vendedor</label>
                 <br />
                 <input type="number" name="Id vendedor" required="true" placeholder="Id Vendedor" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="fecha de despacho">Fecha de Despacho</label>
                 <br />
                 <input type="date" name="fecha de despacho" required="true" placeholder="fecha de despacho" size="50" value="" />
                 <br />
+                </div>
+                <div class = "inputs">
                 <label for="forma de pago">Forma de Pago</label>
                 <br />
+                <select name="forma de pago" id= "forma de pago">
+                    <option>Credito</option>
+                    <option>Contado</option>
+                    </select>
 
-                <button>Contado</button>
-                <button>Credito</button>
+                
                 <br />
+                </div>
+                <button>Registrar</button>
+                
+                </main>
+                </form>
+                
+
                 <div name="table">
+        
 
                     <table class="default">
                         <thead>
