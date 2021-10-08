ba376adcb63b112cd0e2ea568dd1e1c44d6b8d4c
MajoAlba458 <mariaperez.jja@academia.umb.edu.co>
2021-10-07 22:10:30 -0500

    Cambio de table a format

--- reok/src/pages/Products.jsx
+++ reok/src/pages/Products.jsx
@@ -6,43 +6,35 @@ function Products() {
       <div className="Products">
         <Navbar title={'Gestion de Productos'} />
         <section class="productos">
-          <div class="tabla">
-            <table>
-              <tr>
-                <td><h5>Identificador: </h5></td>
-                <td><input type="text" name="fname" maxlength="50" /></td>
-              </tr>
-              <tr>
-                <td><h5>Valor unitario: </h5></td>
-                <td><input type="text" name="fname" maxlength="50" /></td>
-              </tr>
-              <tr>
-                <td><h5>Estado: </h5></td>
-                <td><input type="radio" name="valor" />Disponible</td>
-                <td><input type="radio" name="valor" />No Disponible</td>
-              </tr>
-              <tr>
-                <td><h5>Descripcion: </h5></td>
-                <td>
-                  <textarea name="comments" cols="45" rows="5"></textarea>
-                </td>
-              </tr>
-              <tr></tr>
-              <tr></tr>
-              <tr></tr>
-              <tr>
-                <td>
-                  <button type="submit">Registrar</button>
-                </td>
-                <td>
-                  <button type="submit">Actualizar</button>
-                </td>
-                <td>
-                  <button type="reset">Borrar</button>
-                </td>
-              </tr>
-            </table>
+          <div class="form">
+            <form>
+              <h5>Identificador: </h5>
+              <input type="text" name="fname" maxlength="50" />
+              <h5>Valor unitario: </h5>
+              <input type="text" name="fname" maxlength="50" />
+              <h5>Estado: </h5>
+              <input type="radio" id="b" name="valor" 
+              checked/>
+              <label for="b">Disponible</label>
+              <input type="radio" id="b" name="valor" 
+              checked/>
+              <label for="b">No Disponible</label>
+              <h5>Descripcion: </h5>
+              <textarea name="comments" cols="45" rows="5"></textarea>
+            </form>
+            <div class="botones">
+              <button class="botton" type="submit">
+                Registrar
+              </button>
+              <button class="botton" type="submit">
+                Actualizar
+              </button>
+              <button class="botton" type="reset">
+                Borrar
+              </button>
+            </div>
           </div>
+
           <div name="table" className="sales-table">
             <table class="default">
               <thead>
