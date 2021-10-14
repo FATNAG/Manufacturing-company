81fb617f88dc011ad28852c498c1b3333520710c
alberto hernandez <soloideasmix@gmail.com>
2021-10-13 23:04:44 -0500

    guardando cambios

--- backend-reok/src/controllers/user.controller.js
+++ backend-reok/src/controllers/user.controller.js
@@ -1,24 +1,72 @@
-const userCtrl={}             //creo funcion que obtenga todos los usuarios
+'use strict'   //modo de ejecutar el codigo
+const User=require('./models/user.model')      //importamos el modelo
 
-//crear
-userCtrl.createOne=async(req,res)=>{
-    
-    var user=req.body                //carga en variable el archivo
-    // database.save(user)        // almacenar en DB
-    res.json({msg:'Data received'}) 
+
+function getUser (req,res) {
+    let userId=req.params.userId
+
+    User.findById(userId,(err,user) =>{
+    //retun sale de la funcion si hay error    
+    if (err) return res.status(500).send({message:`Error al realizar consulta:${err}`})
+    if(!user) return res.status(484).send({message:`El usuario no existe`})
+
+    res.status(200).send({user:user})
+   })
 }
 
-//read
-userCtrl.getAll= async(req,res)=>{
-    res.json({msg:'Funciona'})
+function getUsers (req,res) {
+    User.find({}, (err,users)=>{
+        if (err) return res.status(500).send({message:`Error al realizar consulta:${err}`})
+        if(!users) return res.status(484).send({message:`No existen usuarios`})
+       
+        res.send(200,{users:users})       //para probarla, con array [] 
+        
+        })
 }
 
-//update
-userCtrl.editOne= async(req,res)=>{
-    res.json({msg:'Funciona'})
+function saveUser(req,res) {
+    console.log('POST /api/user')
+    console.log(req.body)
+    //almacenar en DB
+    let user=new User()
+    user.usuarioID=req.body.usuarioID
+    user.nombre=req.body.nombre
+    user.apellido=req.body.apellido
+    user.email=req.body.email
+    user.telefono=req.body.telefono
+    user.rol=req.body.rol
 
-//delete
-userCtrl.deleteOne= async(req,res)=>{
-    res.json({msg:'Funciona'})
+    user.save((err,userStored) =>{
+      if(err) res.status (500).send({message:`Error al guardar en DB:${err}`})  
 
-module.exports=userCtrl
+      res.status(200).send({user:userStored})
+    })   //guardar producto y funcion callback
+}
+
+function updateUser (req,res) {
+    let userId=req.params.userId
+    let update=req.body
+    User.findByIdAndUpdate(userId,update,(err, userUpdated) => { //le paso objeto con los campos a actualizar
+    if (err)res.status(500).send({message:`Error al actualizar usuario:${err}`})
+    res.status(200).send({user:userUpdated})
+    })
+}
+function deleteUser (req,res) {
+    let userId=req.params.userId
+    User.findById(userId,(err,user) =>{
+        //retun sale de la funcion si hay error    
+        if (err)res.status(500).send({message:`Error al borar usuario:${err}`})
+        user.remove(err => {  
+            if (err)res.status(500).send({message:`Error al borar usuario:${err}`})
+            res.status(200).send({message:`El usuario se ha borrado`})
+        })
+    }) 
+}
+
+module.exports = {
+    getUser,
+    getUsers,
+    saveUser,
+    updateUser,
+    deleteUser
+}
--- backend-reok/src/index.js
+++ backend-reok/src/index.js
@@ -3,12 +3,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');   
-const User=require('./models/user.model')      //importamos el modelo
 
 // crear servidor
 const app = express();
 const port =process.env.PORT || 3000                 //definir puerto
 const MONGODB_URI='mongodb+srv://admin:1234@cluster0.edu3m.mongodb.net/reok?retryWrites=true&w=majority'
+
+const UserCtrl =require('./controllers/user.controller')    //creo UserCtrl e importo las funciiones
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())                           //agrego el middleware
 
@@ -17,74 +18,23 @@ app.get('/', function (req, res) {
  }); 
 
 //ruta leer todos
-app.get('/api/user',(req,res)=>{
-    User.find({}, (err,users)=>{
-    if (err) return res.status(500).send({message:`Error al realizar consulta:${err}`})
-    if(!users) return res.status(484).send({message:`No existen usuarios`})
-   
-    res.send(200,{users:users})       //para probarla, con array [] 
-    
-    })
-})
+app.get('/api/user', UserCtrl.getUsers)       //llamo la funcion getUsers
  
  //ruta Leer por ID
- app.get('/api/user/:userId',(req,res)=>{
-     let userId=req.params.userId
-
-     User.findById(userId,(err,user) =>{
-     //retun sale de la funcion si hay error    
-     if (err) return res.status(500).send({message:`Error al realizar consulta:${err}`})
-     if(!user) return res.status(484).send({message:`El usuario no existe`})
-
-     res.status(200).send({user:user})
-    })
- })
+ app.get('/api/user/:userId', UserCtrl.getUser)
  
- //ruta tipo Post, enviar datos
- app.post('/api/user/',(req,res)=>{
-    console.log('POST /api/user')
-    console.log(req.body)
-    //almacenar en DB
-    let user=new User()
-    user.usuarioID=req.body.usuarioID
-    user.nombre=req.body.nombre
-    user.apellido=req.body.apellido
-    user.email=req.body.email
-    user.telefono=req.body.telefono
-    user.rol=req.body.rol
-
-    user.save((err,userStored) =>{
-      if(err) res.status (500).send({message:`Error al guardar en DB:${err}`})  
-
-      res.status(200).send({user:userStored})
-    })   //guardar producto y funcion callback
+ //ruta tipo Post, enviar datos -almacenar
+ app.post('/api/user/',UserCtrl.saveUser)
+   
      /* console.log(req.body)                                //para probarla se accede al body (parser)
      //res.send(200,{message:'El usuario se ha recibido'}) // respuesta, envia el status 
      res.status(200).send({message:'El usuario se ha recibido'})  //respuesta, incluye el .status */
- })
- 
+  
  //ruta de actualizaciones por ID
- app.put('/api/user/:userId',(req,res)=>{
-    let userId=req.params.userId
-    let update=req.body
-    User.findByIdAndUpdate(userId,update,(err, userUpdated) => { //le paso objeto con los campos a actualizar
-    if (err)res.status(500).send({message:`Error al actualizar usuario:${err}`})
-    res.status(200).send({user:userUpdated})
-    })
-})
+ app.put('/api/user/:userId',UserCtrl.updateUser)
  
  //ruta borrar por ID
- app.delete('/api/user/:userId',(req,res)=>{
-    let userId=req.params.userId
-    User.findById(userId,(err,user) =>{
-        //retun sale de la funcion si hay error    
-        if (err)res.status(500).send({message:`Error al borar usuario:${err}`})
-        user.remove(err => {  
-            if (err)res.status(500).send({message:`Error al borar usuario:${err}`})
-            res.status(200).send({message:`El usuario se ha borrado`})
-        })
-    }) 
- })
+ app.delete('/api/user/:userId',UserCtrl.deleteUser)
  
 // Conexion a mongo atlas  
 mongoose.connect(MONGODB_URI ||'mongodb://localhost:27017/reok', (err,res) =>{      //conectar a DB
