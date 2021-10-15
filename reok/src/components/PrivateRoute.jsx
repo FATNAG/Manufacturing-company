03c6116e6b29809b7ecf346b76749466f1e8ee07
Jofay-zs <jofayzs19@gmail.com>
2021-10-15 08:00:40 -0500

    Creando estado de carga

--- reok/src/components/PrivateRoute.jsx
+++ reok/src/components/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import BlockingMessage from "./BlockingMessage";
+import Loading from "./Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -9,7 +10,12 @@ const PrivateRoute = ({ children }) => {
     console.log(user, isAuthenticated, isLoading);
   }, [user, isAuthenticated, isLoading]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
   return isAuthenticated ? (
     <>{children}</>
   ) : (
